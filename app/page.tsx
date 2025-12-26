f164310cc2a5ad351191183315d10cd150f58aa3
Shivam Gupta <kapilmohangupta20@gmail.com>
2025-12-26 19:38:17 +0530

    file bug fixed!

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,13 @@ export default function Home() {
   return (
     <div>
       Make hand written notes for next js better!!🐼
-      <div>catches all dynamic routes! , with "[...]"</div>
-      <div>catches one dynamic routes! , with "[]"</div>
-      <div>
-        catches all dynamic routes and gives optional catching for the routes! ,
-        with "[[...]]"
+      <div> 💠  Normal Dynamic routes ~ catches all dynamic routes! , with "[...]"</div>
+      <div> 💠  Catch-all Segments routes (Dynamic Routes Subpart) ~ Catch-all Segments catches one dynamic routes! , with "[]"</div>
+      <div> 💠  
+        Optional Catch-all Segments routes (Dynamic Routes Subpart) ~ catches all dynamic routes and gives optional catching for the routes! ,with "[[...]]"
       </div>
-      <div>skips the folder name in the url! , with "()"</div>
+      <div> 💠  Route Groups skips the folder name in the url! , with "()"</div>
+      <div> 💠  Parallel routes ~ Uses inside layo`ut folder only by imported with children . cannot be accessed by url! , with "@"</div>
     </div>
   );
 }
